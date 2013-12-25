b3e4a89847c0c2a23644ff4661e8885d686776c8
= <= hudson.deary@gmail.com>
2013-12-25 06:26:41 -0600

    modified the header. integrated instafeedjs and got the text on the homescreen to alternate different phrases with colors.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var port = process.env.PORT || 3700;
+var port = process.env.PORT || 0049;
 var app = express();
 app.use(express.logger());
 
@@ -7,3 +7,4 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(port);
 console.log("Mysite is running on localhost:" + port);
+setInterval(function(){ }, 30000);

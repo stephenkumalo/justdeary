d425dcaff69a1b4f3f9e99c25d9680c5a4959914
Deary Hudson <hudson.deary@gmail.com>
2013-12-28 05:51:25 -0600

    Allow CORS

--- server.js
+++ server.js
@@ -5,5 +5,11 @@ app.use(express.logger());
 
 app.use(express.static(__dirname + '/public'));
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.listen(port);
 console.log("Mysite is running on localhost:" + port);
